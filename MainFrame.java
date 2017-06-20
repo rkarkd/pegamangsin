6951746407daf4b9fb580cf2271f0921abc5494a
ParkSeHoo <tkrhktlqdjr1@naver.com>
2017-06-20 19:51:00 +0900

    Update MainFrame.java

--- MainFrame.java
+++ MainFrame.java
@@ -1,190 +1,86 @@
-package racingHorse;
+package kr.koreait.moving;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.Random;
+import java.awt.Checkbox;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class MainFrame extends JFrame {
+public class BtnClass extends JFrame implements ItemListener{
 
-	public static final int WIDTH = 1200;
-	public static final int LANE_SIZE = 60;
+	JCheckBox one,two,three,four,five,six;
+	JPanel controlPanel = new JPanel();
+	JLabel Label = new JLabel("ì´ê³³ì— ì„ íƒí•œ ëª©ë¡ì´ ë‚˜ì˜µë‹ˆë‹¤ ");
+	JButton button1 = new JButton("ì‹œì‘");
 	
-	private int challengerNumber = 6; // Âü°¡ÀÚ ¼ö
-	private RacingPanel racingPanel = new RacingPanel(challengerNumber);
-	private JPanel upPanel = new JPanel();
-	private JPanel rankingPanel = new JPanel();
-	private MenuPanel menuPanel = new MenuPanel(racingPanel);
-	private JPanel broadcastingPanel = new JPanel();
-	private JLabel rankingLabel = new JLabel("¼øÀ§");
-	
-	public MainFrame() {
-		setTitle("°æ¸¶ °ÔÀÓ");
-		setBounds(600, 100, WIDTH, 500 + LANE_SIZE*challengerNumber);
+	public BtnClass () {
 		
-		setLayout(new BorderLayout());
+		String[] menu1 = {"ë‹¨ìŠ¹ì‹(1ë“±ë§ì¶”ê¸°)","ë³µìŠ¹ì‹(1,2ë“± ë§ì¶”ê¸° (ìˆœì„œìƒê´€X))","ìŒìŠ¹ì‹(1,2ë“± ë§ì¶”ê¸° (ìˆœì„œìƒê´€O))"
+				,"ì—°ìŠ¹ì‹(1,2ë“±ì¤‘ í•˜ë‚˜ ë§ì¶”ëŠ”ê±°)","ë³µì—°ìŠ¹ì‹(3ë“±ì•ˆì— 2ë§ˆë¦¬ ë§ì¶”ê¸° ìˆœì„œìƒê´€X)","ì‚¼ë³µìŠ¹ì‹(1,2,3ë“± ë§ë§ì¶”ê¸° ìˆœì„œìƒê´€X)"};
+		String name1 = (String) JOptionPane.showInputDialog(button1, "ë­˜ë¡œ ê±¸ë˜?", "#", JOptionPane.INFORMATION_MESSAGE,new ImageIcon("")
+				, menu1,"ë‹¨ìŠ¹ì‹");
 		
-		upPanel.setLayout(new BorderLayout());
-		upPanel.add(rankingPanel, BorderLayout.WEST);
-		upPanel.add(menuPanel, BorderLayout.CENTER);
 		
-		rankingPanel.setPreferredSize(new Dimension(600, 300));
-		rankingPanel.add(rankingLabel);
-		rankingLabel.setText("¼øÀ§");
-		
-		add(upPanel, BorderLayout.NORTH);
-		add(racingPanel, BorderLayout.CENTER);
-		add(broadcastingPanel, BorderLayout.SOUTH);
-		
+		setTitle("item");
+		setBounds(200,200,500,600);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		one = new JCheckBox("1ë²ˆë§ˆ",false);
+		two = new JCheckBox("2ë²ˆë§ˆ",false);
+		three = new JCheckBox("3ë²ˆë§ˆ",false);
+		four = new JCheckBox("4ë²ˆë§ˆ",false);
+		five = new JCheckBox("5ë²ˆë§ˆ",false);
+		six = new JCheckBox("6ë²ˆë§ˆ",false);
+		controlPanel.add(one);
+		controlPanel.add(two);
+		controlPanel.add(three);
+		controlPanel.add(four);
+		controlPanel.add(five);
+		controlPanel.add(six);
+		
+		add(controlPanel,BorderLayout.NORTH);
+		
+		add(button1);
+		
+		Label.setHorizontalAlignment(JLabel.CENTER);
+		add(Label,BorderLayout.SOUTH);
+		
+		one.addItemListener(this);
+		two.addItemListener(this);
+		three.addItemListener(this);
+		four.addItemListener(this);
+		five.addItemListener(this);
+		six.addItemListener(this);
+		
 		setVisible(true);
 	}
+	
+	
 	
 	public static void main(String[] args) {
-		MainFrame frame = new MainFrame();
-		JFrame frame_2 = new JFrame("title");
-		frame_2.setVisible(true);
-		frame_2.setBounds(200, 500, 400, 500);
+		
+		BtnClass frame = new BtnClass();
+		
 		
 	}
 
-	public void printStatus(){
+
+
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		Object obj = e.getSource();
+		
+//		System.out.println(((JCheckBox)obj).getText());
+		JCheckBox item= ((JCheckBox)obj);
+//		isSelected() : ì²´í¬ìƒìê°€ ì„ íƒëœ ìƒíƒœë©´ true í•´ì œëœ ìƒíƒœë©´ falseë¥¼ ë¦¬í„´í•œë‹¤. 
+		Label.setText(item.getText()+(item.isSelected() ? "ì„ íƒ" : "í•´ì œ"));
 		
 	}
-}
-
-class RacingPanel extends JPanel implements Runnable {
-
-	public static final int START_POINT = 0; // Ãâ¹ßÁöÁ¡
-	public static final int FINISH_POINT = 850; // µµÂøÁöÁ¡
 	
-	private Horse[] horses;
-
-	public RacingPanel(int challengerNumber) {
-		horses = new Horse[challengerNumber];
-		for(int i = 0; i < challengerNumber; i++){
-			horses[i] = new Horse((i+1)+"¹ø¸¶", START_POINT, i *MainFrame.LANE_SIZE);
-		}
-		setPreferredSize(new Dimension(MainFrame.WIDTH, MainFrame.LANE_SIZE* challengerNumber));
-		
-
-		setVisible(true);
-	}
-
-	@Override
-	public void paint(Graphics g) {
-//		¹è°æ ±×¸®±â
-		for(int i = 0; i < horses.length; i++){
-			g.setColor(new Color((i % 2 == 0 ? 183 : 0), (i % 2 == 0 ? 255 : 204), (i % 2 == 0 ? 190 : 19)));
-			g.fillRect(0, i *MainFrame.LANE_SIZE , MainFrame.WIDTH, (i+1)*MainFrame.LANE_SIZE);
-		}
-		g.setColor(new Color(255,255,255));
-		g.fillRect(0,  (horses.length) *MainFrame.LANE_SIZE , MainFrame.WIDTH, (horses.length)*MainFrame.LANE_SIZE);
-		
-		
-//		¸» ±×¸®±â
-		int horseSize= 60;
-		g.setColor(Color.PINK);
-		for(int i = 0; i < horses.length; i++){
-//			¸» °¢°¢ÀÇ À§Ä¡¸¦ ¹Ş¾Æ¿Í¼­ ±×¸°´Ù.
-			try{
-				g.fillOval(horses[i].getPosition_X(), i *MainFrame.LANE_SIZE, 60, MainFrame.LANE_SIZE);
-			}catch(NullPointerException e){
-				e.printStackTrace();
-			}
-		}
-	}
-
-	@Override
-	public void run() {
-		while (true) {
-			if (isAllHorseFinished()) {
-				break;
-			}
-			for(int i = 0; i < horses.length ; i++){
-				if(horses[i].getPosition_X() <= FINISH_POINT){
-					horses[i].move(new Random().nextInt(6)+1);
-				}else{
-					
-				}
-			}
-			repaint();
-			try { Thread.sleep(20); } catch (InterruptedException e) { e.printStackTrace();}
-		}
-	}
-
-	private boolean isAllHorseFinished() {
-		for(int i = 0; i < horses.length ; i++){
-			if(horses[i].getPosition_X() < FINISH_POINT){
-				return false;
-			}
-		}
-		for(int i = 0; i < horses.length ; i++){
-//			À§Ä¡ ÃÊ±âÈ­
-			horses[i].setPosition_X(START_POINT);
-		}
-		return true;
-	}
-
-	public Horse[] getHorses() {
-		return horses;
-	}
-
-}
-
-class MenuPanel extends JPanel implements ActionListener{
-//	private String[] horseNames = {"1¹ø¸¶", "2¹ø¸¶", "3¹ø¸¶", "4¹ø¸¶", "5¹ø¸¶", "6¹ø¸¶"};
-//	private JComboBox horseList1 = new JComboBox(horseNames);
-//	private JComboBox horseList2 = new JComboBox(horseNames);
-	private RacingPanel racingPanel;
-	private String[] horseNames;
-	private JComboBox horseList1;
-	private JComboBox horseList2;
-	private JButton selectButton = new JButton("¼±ÅÃ ¿Ï·á");
-	
-	public MenuPanel(RacingPanel racingPanel){
-		this.racingPanel = racingPanel;
-		horseNames = new String[racingPanel.getHorses().length + 1];
-		horseNames[0] = "¼±ÅÃ¾ÈÇÔ";
-		for(int i = 0; i < racingPanel.getHorses().length; i++){
-			horseNames[i+1] = racingPanel.getHorses()[i].getName();
-		}
-		
-		horseList1 = new JComboBox(horseNames);
-		horseList2 = new JComboBox(horseNames);
-		
-		setPreferredSize(new Dimension(600,  300));
-		setLayout(new GridLayout(2,3));
-		
-		add(horseList1);
-		add(horseList2);
-		add(new JLabel("³²Àº ±İ¾× : 1000¿ø"));
-		add(new JLabel("¹èÆÃ¾× : 100¿ø"));
-		add(new JLabel(" "));
-		add(selectButton);
-		
-		selectButton.addActionListener(this);
-	}
-
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		
-		Thread thread = new Thread(racingPanel);
-		thread.start();
-//			selectButton.setEnabled(false);
-//			JOptionPane.showMessageDialog(null, horseList1.getSelectedItem() + "¸¦ ¼±ÅÃÇÏ¼Ì½À´Ï´Ù.");
-			
-	}
 }
