e5da38bf24fac5a35e1e0e13be342be11e36c443
HaenaraShin <hamster12345@gmail.com>
2017-06-21 04:05:22 +0900

    Update User.java

--- User.java
+++ User.java
@@ -2,4 +2,17 @@ package racingHorse;
 
 public class User {
 
+	private String name = "무명씨";
+	private int balance = 1000;
+	
+	public String getName() {
+		return name;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
 }
