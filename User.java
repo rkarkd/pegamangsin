890e25185df5afb6744c2a502cb5bf0e844bf250
HaenaraShin <hamster12345@gmail.com>
2017-06-23 19:08:32 +0900

    Update User.java

--- User.java
+++ User.java
@@ -5,6 +5,12 @@ public class User {
 	private String name = "무명씨";
 	private int balance = 1000;
 	
+	public User(){ }
+	public User(String name, int balance) {
+		this.name = name;
+		this.balance = balance;
+	}
+	
 	public String getName() {
 		return name;
 	}
@@ -13,6 +19,10 @@ public class User {
 	}
 	public void setBalance(int balance) {
 		this.balance = balance;
+	}
+	
+	public void addBalance(int val) {
+		balance += val;
 	}
 	
 }
